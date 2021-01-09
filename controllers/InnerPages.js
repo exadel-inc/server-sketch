c7a4eb867af8031b5034e68457732cf610b346c4
dmantsevich <dmantsevich@exadel.com>
2021-01-10 02:05:33 +0300

    https://github.com/exadel-inc/server-sketch/issues/4
    4. Updates for InnerPages.js

--- controllers/InnerPages.js
+++ controllers/InnerPages.js
@@ -10,18 +10,22 @@ const glob = require('glob');
 module.exports = function (folderPath) {
   if (folderPath.endsWith('.html')) {
     folderPath = dirname(folderPath);
-    if (!folderPath.endsWith('/')) folderPath += '/';
+    if (!folderPath.endsWith('/')) {
+      folderPath += '/';
+    }
   }
   return glob.sync('*', {
     cwd: `${config.serverViews}/pages/${folderPath}`, // Get all sub folders and HTML pages
     mark: true
   }).map((file) => {
-    if (file.startsWith('_')) return null; // Ignore files, started with "_" (but they are available directly)
+    if (file.startsWith('_')) {
+      return null; // Ignore files, started with "_" (but they are available directly)
+    }
     const isDir = file.endsWith('/');
     return {
       title: startCase(file.replace(/\.html$/, '')), // User friendly format
       isDir: isDir,
       href: isDir ? `${folderPath}${file}${config.defaultPageUrl}` : `${folderPath}${file}` // link to page
     };
-  }).filter(v => !!v);
+  }).filter(Boolean);
 };
